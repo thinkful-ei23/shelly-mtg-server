12d7c68268333955ace40eacdbbb387974002b8b
Shelly Leung <pontificable@yahoo.com>
2018-09-20 15:56:49 -0700

    fixed server.js

--- server.js
+++ server.js
@@ -4,17 +4,13 @@ const cors = require('cors');
 const knex = require('./knex');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-knex');
+// const { dbConnect } = require('./db-knex');
 
 // Create an Express application
 const app = express();
 
 // Log all requests
-app.use(
-	morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-		skip: (req, res) => process.env.NODE_ENV === 'test'
-	})
-);
+app.use(morgan('dev'));
 
 // Create a static webserver
 app.use(express.static('public'));
@@ -89,20 +85,10 @@ app.use((err, req, res, next) => {
 });
 
 // Listen for incoming connections
-function runServer(port = PORT) {
-  const server = app
-    .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
-    })
-    .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
-    });
-}
-
-if (require.main === module) {
-  dbConnect();
-  runServer();
-}
-
-module.exports = { app };
+app
+	.listen(PORT, function() {
+		console.info(`Server listening on ${this.address().port}`);
+	})
+	.on('error', err => {
+		console.error(err);
+	});
