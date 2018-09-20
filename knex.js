e57266c8e51ebfd33282e716af37544aedc10a30
Shelly Leung <pontificable@yahoo.com>
2018-09-20 15:46:12 -0700

    pick and play server

--- knex.js
+++ knex.js
@@ -1,3 +1,30 @@
-const knexConfig = require('./knexfile');
-const environment = process.env.ENVIRONMENT || 'development';
-module.exports = require('knex')(knexConfig[environment]);
+// const knexConfig = require('./knexfile');
+// const environment = process.env.ENVIRONMENT || 'development';
+// module.exports = require('knex')(knexConfig[environment]);
+
+const createKnex = require('knex');
+
+const { DATABASE_URL } = require('./config');
+
+let knex = null;
+
+function dbConnect(url = DATABASE_URL) {
+	knex = createKnex({
+		client: 'pg',
+		connection: url
+	});
+}
+
+function dbDisconnect() {
+	return knex.destroy();
+}
+
+function dbGet() {
+	return knex;
+}
+
+module.exports = {
+	dbConnect,
+	dbDisconnect,
+	dbGet
+};
--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const cors = require('cors');
 const knex = require('./knex');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
+const { dbConnect } = require('./db-knex');
 
 // Create an Express application
 const app = express();
 
 // Log all requests
-app.use(morgan('dev'));
+app.use(
+	morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+		skip: (req, res) => process.env.NODE_ENV === 'test'
+	})
+);
 
 // Create a static webserver
 app.use(express.static('public'));
@@ -84,10 +89,20 @@ app.use((err, req, res, next) => {
 });
 
 // Listen for incoming connections
-app
-	.listen(PORT, function() {
-		console.info(`Server listening on ${this.address().port}`);
-	})
-	.on('error', err => {
-		console.error(err);
-	});
+function runServer(port = PORT) {
+  const server = app
+    .listen(port, () => {
+      console.info(`App listening on port ${server.address().port}`);
+    })
+    .on('error', err => {
+      console.error('Express failed to start');
+      console.error(err);
+    });
+}
+
+if (require.main === module) {
+  dbConnect();
+  runServer();
+}
+
+module.exports = { app };
